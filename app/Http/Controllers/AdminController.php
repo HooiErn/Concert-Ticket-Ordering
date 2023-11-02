54292e1987c2587e05c3820fb74fb506b0df5f8a
HooiErn <d200217c.sc.edu.my>
2023-11-02 13:04:14 +0800

    ticket_type function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use DB;
 use Session;
+use App\Models\User;
+use App\Models\Ticket;
+use App\Models\Ticket_type;
+use App\Models\Order;
+use App\Models\Concert;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
 
@@ -27,11 +32,36 @@ class AdminController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
-        $category = Category::create([
+        $categories = Ticket_type::create([
             'name' => $request -> c_name,
         ]);
 
         Toastr::success('A new ticket category has been added', 'Add Successfully', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+        return redirect('admin/category');
+    }
+
+     //Update Category
+     public function UpdateCategory(Request $request){
+        if (Ticket_type::where('name', $request->name)->exists()) {
+            Toastr::error('This category name already exists. Please try again.', 'Repeat name', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+            return redirect()->back()->withInput()->withErrors(['edit_c_name' => 'Duplicate name has been used.']);
+        }
+        
+        $categories = Ticket_type::where('id',$request -> catID)->first();
+
+        $categories -> name = $request -> name;
+        $categories -> save();
+       
+        Toastr::success('A new ticket category has been updated', 'Updated Successfully', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
+        return redirect('admin/category');
+    }
+
+     //Delete Category
+     public function DeleteCategory(Request $request){
+        $categories = Ticket_type::where('id',$id)->first();
+        $categories -> delete();
+
+        Toastr::success('A ticket category has been deleted', 'Deleted Successfully', ["progressBar" => true, "debug" => true, "newestOnTop" =>true, "positionClass" =>"toast-top-right"]);
         return redirect('admin/category');
     }
 }
---
+++ resources/views/admin/category/UpdateTicketType.blade.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>Edit Category</title>
+        @if(Auth::user()->isAdmin())
+            <script type="text/javascript">
+                window.location.href="{{url('admin/login')}}"
+            </script>
+        @endif
+    </head>
+    <body>
+        <div class="container">
+            <form action="{{ route('category.update') }}" method="POST">
+                @csrf
+
+                @foreach($categories as $category)
+                    <div class="form-group">
+                        <label for="categoryName">Category Name</label>
+                        <input type="text" class="form-control" id="name" name="name" value="{{$category->name}}">
+                        <input type="hidden" class="form-control" id="categoryID" name="categoryID" value="{{$category->id}}">
+                    </div>
+                @endforeach
+                
+                <button class="btn btn-primary" type="submit">Submit</button>
+                @if($errors->any())
+                    @foreach($errors->all() as $error)
+                        <p class="text-danger">{{$error}}</p>
+                    @endforeach
+                @endif
+            </form>
+        </div>
+    </body>
+</html>
