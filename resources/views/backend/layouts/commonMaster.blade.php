611d6da2984240217320e2fe223397ed2c9686bc
HooiErn <d200217c.sc.edu.my>
2023-12-03 17:50:47 +0800

    solve the topbar cant show logout

--- resources/views/backend/layouts/commonMaster.blade.php
+++ resources/views/backend/layouts/commonMaster.blade.php
@@ -25,7 +25,11 @@
     <!-- jquery cdn -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    </head>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</head>
 
     <body id="page-top">
         <!-- Page Wrapper -->
