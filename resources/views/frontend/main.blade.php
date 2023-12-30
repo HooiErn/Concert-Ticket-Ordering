e6f30778a6bb2752ac5d21eb8e35560e8b5123c2
HooiErn <d200217c.sc.edu.my>
2023-12-30 19:47:56 +0800

    edit some word

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -14,7 +14,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="hero-slides-content text-center">
-                                <h6 data-animation="fadeInUp" data-delay="100ms">Latest album</h6>
+                                <h6 data-animation="fadeInUp" data-delay="100ms">Latest Concerts</h6>
                                 <h2 data-animation="fadeInUp" data-delay="300ms">Beyond Time <span>Beyond Time</span></h2>
                                 <a data-animation="fadeInUp" data-delay="500ms" href="#"
                                     class="btn oneMusic-btn mt-50">Discover <i class="fa fa-angle-double-right"></i></a>
@@ -33,8 +33,8 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="hero-slides-content text-center">
-                                <h6 data-animation="fadeInUp" data-delay="100ms">Latest album</h6>
-                                <h2 data-animation="fadeInUp" data-delay="300ms">Colorlib Music <span>Colorlib Music</span>
+                                <h6 data-animation="fadeInUp" data-delay="100ms">Latest Album</h6>
+                                <h2 data-animation="fadeInUp" data-delay="300ms">Melody Moments<span>Melody Moments</span>
                                 </h2>
                                 <a data-animation="fadeInUp" data-delay="500ms" href="#"
                                     class="btn oneMusic-btn mt-50">Discover <i class="fa fa-angle-double-right"></i></a>
