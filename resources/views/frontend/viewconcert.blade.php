6f623dd76be13d7517c6f3c2c60df386eaf6a73e
HooiErn <d200217c.sc.edu.my>
2023-12-30 20:31:17 +0800

    add carousel and edit the button css

--- resources/views/frontend/viewconcert.blade.php
+++ resources/views/frontend/viewconcert.blade.php
@@ -3,19 +3,22 @@
 @section('content')
 
 <!-- Begin Concert Information -->
-<div class="container concert-information ">
+<div class="container concert-information">
     <div class="card shadow">
         <div class="container-fluid">
             <div class="wrapper row">
                 <div class="preview col-md-6">
-                    <div class="preview-pic tab-content">
-                        @foreach(json_decode($concerts->images) as $index => $image)
-                            <div class="tab-pane active" id="pic-1"><img src="{{ asset('images/' . $image) }}" /></div>
-                            @if($index === 0) {{-- Break out of the loop after the first iteration --}}
-                                    @break
-                                @endif
+                    <div id="image-carousel" class="carousel slide">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            @foreach(json_decode($concerts->images) as $index => $image)
+                                <div class="item {{ $index === 0 ? 'active' : '' }}">
+                                    <img src="{{ asset('images/' . $image) }}" alt="Slide {{ $index + 1 }}"
+                                     class="img-responsive" style="height: 250px; width: 500px; object-fit: contain;">
+                                </div>
                             @endforeach
-                        {{-- <div class="tab-pane" id="pic-2"><img src="http://placekitten.com/400/252" /></div>
+                        </div>
+                    <!-- {{-- <div class="tab-pane" id="pic-2"><img src="http://placekitten.com/400/252" /></div>
                         <div class="tab-pane" id="pic-3"><img src="http://placekitten.com/400/252" /></div>
                         <div class="tab-pane" id="pic-4"><img src="http://placekitten.com/400/252" /></div>
                         <div class="tab-pane" id="pic-5"><img src="http://placekitten.com/400/252" /></div> --}}
@@ -43,18 +46,31 @@
                         <li>
                             <a data-target="#pic-5" data-toggle="tab"><img src="http://placekitten.com/200/126" /></a>
                         </li>
-                    </ul> --}}
+                    </ul> --}} -->
 
-                    <ul class="preview-thumbnail nav nav-tabs">
-                        @foreach(json_decode($concerts->images) as $index => $image)
-                            <li class="{{ $index === 0 ? 'active' : '' }}">
-                                <a data-target="#pic-{{ $index + 1 }}" data-toggle="tab">
-                                    <img src="{{ asset('images/' . $image) }}" />
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#image-carousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#image-carousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
 
+                        <!-- Thumbnail navigation -->
+                        <ul class="preview-thumbnail nav nav-tabs">
+                            @foreach(json_decode($concerts->images) as $index => $image)
+                                <li data-target="#image-carousel" data-slide-to="{{ $index }}"
+                                    class="{{ $index === 0 ? 'active' : '' }} d-flex justify-content-center align-items-center">
+                                    <a>
+                                        <img src="{{ asset('images/' . $image) }}" alt="Thumbnail {{ $index + 1 }}"
+                                         class="img-responsive" style="height: 50px; width: 50px; object-fit: contain;">
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
                 <div class="details col-md-6">
                     <h3 class="product-title">{{ $concerts->name }}</h3>
@@ -73,18 +89,11 @@
                     <p class="vote"><strong>Venue</strong>: <strong>{{ $concerts->venue }}</strong></p>
                     <h5 class="sizes">Organizer Name:
                         <span class="size" data-toggle="tooltip" title="small">{{ $concerts->organizer_name }}</span>
-                        {{-- <span class="size" data-toggle="tooltip" title="medium">m</span>
-                        <span class="size" data-toggle="tooltip" title="large">l</span>
-                        <span class="size" data-toggle="tooltip" title="xtra large">xl</span> --}}
                     </h5>
 
-                    <a href="{{ url('/bookingConcert/' .$concerts->id)}}"><button type="submit" class="btn bg-cart"><i class="fa fa-cart-plus mr-2"></i>Book Now</button></a>
-
-                    {{-- <h5 class="colors">colors:
-                        <span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
-                        <span class="color green"></span>
-                        <span class="color blue"></span>
-                    </h5> --}}
+                    <a href="{{ url('/bookingConcert/' .$concerts->id)}}"><button type="submit" class="btn bg-cart btn-lg"
+                    style="transition: background-color 0.3s;"onmouseover="this.style.backgroundColor='#ff9f1a';" onmouseout="this.style.backgroundColor='';">
+                    <i class="fa fa-cart-plus mr-2"></i>Book Now</button></a>
 
                 </div>
             </div>
