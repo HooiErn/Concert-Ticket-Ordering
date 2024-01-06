e962e6933a5dd9efab0dd7e7399e771f380c4fbd
HooiErn <d200217c.sc.edu.my>
2024-01-06 20:48:50 +0800

    update

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,10 +9,10 @@ class Order extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'order_id',
-        'username',
+        'user_id',
+        'user_name',
+        'user_email',
         'concert_name',
-        'order_date',
         'total amount',
         'payment_status',
     ];
