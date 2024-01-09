05af64e6390849d580549a27d3bad8eae02513a8
xiangyi1106 <xiangyicheng03@gmail.com>
2024-01-09 22:54:48 +0800

    minor fix

--- resources/views/backend/layouts/sections/modal/logout_modal.blade.php
+++ resources/views/backend/layouts/sections/modal/logout_modal.blade.php
@@ -1,4 +1,4 @@
-<!-- Logout Modal-->
+<!-- Logout Modal -->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
     <div class="modal-dialog" role="document">
